3bd94ecf0fe9da21b5c97eaf8130032c8f19558b
Geier0x413 <geier0x413@gmail.com>
2024-07-26 18:45:00 -0400

    Updated app with basic functionality to generate barcodes based on input

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+const tag = {
+  "aisle": document.querySelector( "#tag-aisle" ),
+  "bay": document.querySelector( "#tag-bay" ),
+  "generate": document.querySelector( "#generate" ),
+};
+
 function baytag( aisle , bay ) {
-  // Add checks for properly formatted aisle and bay 00-000
+  aisle = `${ aisle }`.toUpperCase().padStart( 2 , "0" );
+  bay = `${ bay }`.toUpperCase().padStart( 3 , "0" );
+
   JsBarcode( "#barcode" , `9809000000-${ aisle }-${ bay }` , {
     background: "#ffff00",
     displayValue: false,
@@ -8,6 +16,32 @@ function baytag( aisle , bay ) {
     lineColor: "#000000",
     width: 2
   } );
+
+  return { "aisle": aisle , "bay": bay };
 }
 
-baytag( "05" , "008" );
+// aisle.addEventListener( "input" , function( event ) {
+//   console.log( event.data );
+//   this.format( event.data );
+//   baytag( this.value , bay.value );
+// } );
+
+// bay.addEventListener( "input" , function( event ) {
+//   console.log( event.data );
+//   this.format( event.data );
+//   baytag( aisle.value , this.value );
+// } );
+
+generate.addEventListener( "click" , function() {
+  const { aisle , bay } = baytag( tag.aisle.value , tag.bay.value );
+  tag.aisle.value = aisle;
+  tag.bay.value = bay;
+} );
+
+for ( let i = 1; i <= 65; i++ ) {
+  for ( let j = 1; j <= 22; j++ ) {
+    setTimeout( () => {
+      baytag( i , j  );
+    } , 16 * i );
+  }
+}
