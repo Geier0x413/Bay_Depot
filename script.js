1cb675d0af03c1ec6424531c05db589f2e31b4e7
Geier0x413 <geier0x413@gmail.com>
2024-07-26 09:10:50 -0400

    Testing if script updated and works properly

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-alert( "Script loaded!" );
-
-window.addEventListener( "load" , () => {
-  JsBarcode( "#barcode" , "9809000000-05-002" , {
-    background: "#ff0000",
-    displayValue: false,
-    format: "CODE128",
-    height: 64,
-    lineColor: "#000000",
-    width: 1
-  } );
+JsBarcode( "#barcode" , "9809000000-05-002" , {
+  background: "#ffff00",
+  displayValue: false,
+  format: "CODE128",
+  height: 64,
+  lineColor: "#000000",
+  width: 1
 } );
