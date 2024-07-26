8a284c59069dfb9d528f8903969e4e56cae5033a
Geier0x413 <geier0x413@gmail.com>
2024-07-26 09:26:17 -0400

    Implemented basic structure for generating baytags

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-JsBarcode( "#barcode" , "9809000000-05-002" , {
-  background: "#ffff00",
-  displayValue: false,
-  format: "CODE128",
-  height: 100,
-  lineColor: "#000000",
-  width: 2
-} );
+function baytag( aisle , bay ) {
+  // Add checks for properly formatted aisle and bay 00-000
+  JsBarcode( "#barcode" , `9809000000-${ aisle }-${ bay }` , {
+    background: "#ffff00",
+    displayValue: false,
+    format: "CODE128",
+    height: 100,
+    lineColor: "#000000",
+    width: 2
+  } );
+}
+
+baytag( "05" , "008" );
