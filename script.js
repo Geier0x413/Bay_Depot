0c93c4137218ca193a8882330b3e0ec6c78226ee
Geier0x413 <geier0x413@gmail.com>
2024-07-26 09:18:28 -0400

    Finished testing if script loads and works properly

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ JsBarcode( "#barcode" , "9809000000-05-002" , {
   background: "#ffff00",
   displayValue: false,
   format: "CODE128",
-  height: 64,
+  height: 100,
   lineColor: "#000000",
-  width: 1
+  width: 2
 } );
